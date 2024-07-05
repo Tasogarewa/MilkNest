c9924ba854ca017e20d2982c11c95333563d8eb4
Артем Шафранівський <Shafranivskyi.A.O@nmu.one>
2024-07-05 17:32:55 +0300

    Created structure of application and added entities for future using

---
+++ MilkNest.Domain/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MilkNest.Domain
+{
+    public class User
+    {
+       
+            public int Id { get; set; }
+            public string UserName { get; set; }
+            public string Email { get; set; }
+            public string PasswordHash { get; set; }
+            public bool IsAdmin { get; set; }
+            public DateTime DateRegistered { get; set; }
+            public bool IsOnline { get; set; }
+
+            public ICollection<Order> Orders { get; set; }
+            public ICollection<Comment> Comments { get; set; }
+        
+    }
+}
